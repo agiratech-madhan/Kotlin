548c52a15aa3ed2a93d0dc38e9a4ca42197b9ebb
agiratech-madhan <madhan.k@agiratech.com>
2023-06-23 17:53:35 +0530

    Kotlin fundamentals

---
+++ app/src/main/java/com/example/kotlinbasics/Main.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinbasics
+
+fun main(){
+    val x=8;
+    print("sample ")
+    var byte:Byte=15;//8bit
+    var shortValue:Short=15777;//16bit ,//int 32 bit,Long64
+    var myInt:Int=788888899;
+    var myLonng:Long=22_334_343_4_34_7;
+    var myFloat:Float=13.5F;
+    var sam:Double=23232323.23232323
+    println(x);
+}
