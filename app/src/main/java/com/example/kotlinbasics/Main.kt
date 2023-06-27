dc477f6581447e67736f1204cb9296b55b9b8cee
agiratech-madhan <madhan.k@agiratech.com>
2023-06-27 11:00:27 +0530

    interfaces

--- app/src/main/java/com/example/kotlinbasics/Main.kt
+++ app/src/main/java/com/example/kotlinbasics/Main.kt
@@ -1,67 +1,91 @@
 package eu.tutorials.helloworld
 
 
-// Inheritance is one of the main concepts of
-// Object Oriented Programming (OOP).
-///It allows a class to inherit features
-//(properties and methods) from another class and reuse them.
-// The class that inherits the features of another
-// class is called the Sub class or Child class or
-// Derived class , and the class whose features are
-// inherited is called the Super class or Parent class
-// or Base class.
-// All the classes in Kotlin inherit from a class called Any. It corresponds to the Object class in Java.
-// Every class that you create in Kotlin implicitly
-// inherits from Any -
+/*
+https://kotlinlang.org/docs/tutorials/kotlin-for-py/inheritance.html#polymorphism
 
-class Person // Implicitly inherits from the default Parent class - Any
-// The Any class contains three methods toString(),
-// equals() and hashCode().
-// All classes in Kotlin inherit these three methods
-// from Any, and can override them in order to
-// provide their own implementation.
+"The single-parent rule often becomes too limiting,
+as you'll often find commonalities between classes in
+different branches of a class hierarchy.
+These commonalities can be expressed in interfaces.
 
-// Base class (Super class)
-open class Vehicle {
+An interface is essentially a contract that a
+class may choose to sign; if it does, the class
+is obliged to provide implementations of the properties
+and functions of the interface.
+However, an interface may (but typically doesn't)
+provide a default implementation of some or all
+of its properties and functions. If a property or
+function has a default implementation,
+the class may choose to override it,
+but it doesn't have to.
+*/
+
+interface Driveable {
+    val maxSpeed: Double
+    fun drive(): String
+    fun brake(){
+        println("The drivable is braking")
+    }
 }
 
-
-
-open class Car(val name: String, val brand: String) {
+// Class Car which extends the interface
+open class Car(override val maxSpeed: Double,
+               open val brandName: String
+) : Driveable {
     // open so it can be overriden by inhereting classes
     open var range: Double = 0.0
-    fun extendRange(amount: Double) {
-        if(amount > 0) {
+
+    open fun extendRange(amount: Double) {
+        if (amount > 0) {
             range += amount
         }
+
     }
-    open fun drive(distance: Double){
-        println("Drove for $distance KM")
+
+    override fun drive(): String {
+        println("Drove for $range KM")
+        return range.toString()
+    }
+
+    // overridden functions are implicitly open:
+    override fun brake() {
+        println("The car is breaking")
     }
 }
+// In case there is no primary Constructor
+class ElectricCar(override val maxSpeed: Double,
+                  override val brandName: String,
+                  batteryLife: Double) : Car(maxSpeed, brandName) {
 
-class ElectricCar(name: String, brand: String, batteryLife: Double) : Car(name, brand) {
-
-    var chargerType = "Type1"
     // in KM
     override var range = batteryLife/6
 
-    override fun drive(distance: Double){
+    override fun drive() = "Overriding the drive of my Car"
+
+    fun drive(distance: Double){
         println("Drove for $distance KM on electricity")
+    }
+    override fun brake(){
+        println("The electirc car is breaking")
     }
 }
 
+
+
 fun main(args: Array<String>){
-    var audiA3 = Car("A3", "Audi")
-    var teslaS = ElectricCar("S-Model", "Tesla", 85.0)
+    var audiA3 = Car(200.0, "Audi")
+    var teslaS = ElectricCar(250.0, "Tesla", 85.0)
 
     // Polymorphism is the ability to treat objects
     // with similar traits in a common way
-    audiA3.drive(200.0)
+    audiA3.drive()
     // Only works because ElectricCar is a Subclass of Car
-    // Or alternativly works if Car was a Interface and Electriccar would inherit from it
+    // Or alternatively works if Car was an Interface and ElectricCar would inherit from it
+    teslaS.drive()
     teslaS.drive(200.0)
 }
-
-
+// Subclasses of a class that implements an interface
+// (in this case, ElectricCar) are also considered
+// to be implementing the interface.
 
