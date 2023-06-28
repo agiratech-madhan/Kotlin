697f00745a3bb54f3b7381b6938bce398a84b2b7
agiratech-madhan <madhan.k@agiratech.com>
2023-06-28 14:01:13 +0530

    Enums

--- app/src/main/java/com/example/kotlinbasics/Main.kt
+++ app/src/main/java/com/example/kotlinbasics/Main.kt
@@ -1,65 +1,43 @@
 package eu.tutorials.interfacestest
 
-// An abstract class cannot be instantiated
-// (you cannot create objects of an abstract class).
-// However, you can inherit subclasses from can them.
-// The members (properties and methods) of an abstract class are non-abstract
-// unless you explicitly use the abstract keyword to make them abstract.
-abstract class Mammal(val name: String, val origin: String,
-                      val weight: Double) {   // Concrete (Non Abstract) Properties
+interface ICardCashBack {
+    fun getCashbackValue(): Float
+}
 
-    // Abstract Property (Must be overridden by Subclasses)
-    abstract var maxSpeed: Double
-
-    // Abstract Methods (Must be implemented by Subclasses)
-    abstract fun run()
-    abstract fun breath()
-
-    // Concrete (Non Abstract) Method
-    fun displayDetails() {
-        println("Name: $name, Origin: $origin, Weight: $weight, " +
-                "Max Speed: $maxSpeed")
+enum class CreditCardType(val color: String, val maxLimit: Int = 1000000): ICardCashBack {
+    SILVER("gray", 50000) {
+        override fun getCashbackValue(): Float = 0.02f
+    },
+    GOLD("gold"){
+        override fun getCashbackValue(): Float = 0.04f
+    },
+    PLATINUM("black"){
+        override fun getCashbackValue(): Float = 0.06f
     }
 }
 
-class Human(name: String, origin: String, weight: Double,
-            override var maxSpeed: Double): Mammal(name, origin, weight) {
-
-    override fun run() {
-        // Code to run
-        println("Runs on two legs")
-    }
-
-    override fun breath() {
-        // Code to breath
-        println("Breath through mouth or nose")
-    }
-}
-
-class Elephant(name: String, origin: String, weight: Double,
-               override var maxSpeed: Double): Mammal(name, origin, weight) {
-
-    override fun run() {
-        // Code to run
-        println("Runs on four legs")
-    }
-
-    override fun breath() {
-        // Code to breath
-        println("Breath through the trunk")
-    }
-}
 
 fun main() {
-    val human = Human("Denis", "Russia",
-        70.0, 28.0)
-    val elephant = Elephant("Rosy", "India",
-        5400.0, 25.0)
 
-    human.run()
-    elephant.run()
+    /* Access properties and methods */
+    println(CreditCardType.SILVER.color)    // gray
+    println(CreditCardType.SILVER.getCashbackValue())   // 0.02
 
-    human.breath()
-    elephant.breath()
+    /* Enum constants are objects of enum class type. */
+    val peterCardType: CreditCardType = CreditCardType.GOLD
 
+    /* Each enum object has two properties: ordinal and name */
+    println(CreditCardType.GOLD.ordinal)
+    println(CreditCardType.GOLD)    // OR CreditCardType.GOLD.name
+
+    /* Each enum object has two methods: values() and valueOf() */
+    val myConstants: Array<CreditCardType> = CreditCardType.values()
+    myConstants.forEach { println(it) }
+
+    /* Using in 'when' statement */
+    when(peterCardType) {
+        CreditCardType.SILVER -> println("Peter has silver card")
+        CreditCardType.GOLD -> println("Peter has gold card")
+        CreditCardType.PLATINUM -> println("Peter has platinum card")
+    }
 }
